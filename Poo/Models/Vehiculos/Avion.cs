53f53186dba97d094052686942ddb62e038c64df
Joaquin Tealdi <joaquintealdi25@gmail.com>
2023-10-23 21:33:52 -0300

    add vehicles files

---
+++ Poo/Models/Vehiculos/Avion.cs
@@ -0,0 +1,36 @@
+﻿using Poo.Models.Vehiculos.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo.Models.Vehiculos
+{
+    internal class Avion : IAvion
+    {
+        public Avion()
+        {
+                
+        }
+        public void Apagar()
+        {
+            Console.WriteLine("Avion apagado");
+        }
+
+        public void Aterrizar()
+        {
+            Console.WriteLine("Avion aterrizando");
+        }
+
+        public void Despegar()
+        {
+            Console.WriteLine("Avion despegando");
+        }
+
+        public void Encender()
+        {
+            Console.WriteLine("Avion encendido");
+        }
+    }
+}
